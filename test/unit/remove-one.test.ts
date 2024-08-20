780a43f2cf7817229a8b7aa90ab098f5b5e245df
Jon Higger <jonathan.higger@gmail.com>
2024-08-20 16:14:34 -0600

    added beforeMutate test for remove-one

--- test/unit/remove-one.test.ts
+++ test/unit/remove-one.test.ts
@@ -36,6 +36,35 @@ describe("mutate.up", () => {
   });
 });
 
+describe("mutate.beforeMutate", () => {
+  it("should throw an error if referencing a path that's already there", async () => {
+    const beforeMutateResult = await Promise.resolve()
+      .then(() => {
+        const a = mutators
+          .removeOne<{ name: string }, "name">("name")
+          .beforeMutate({
+            paths: [],
+          });
+        console.log(a);
+      })
+      .catch((e) => e);
+
+    expect(beforeMutateResult).toBeInstanceOf(Error);
+    expect(beforeMutateResult.message).toBe(`Path name not found`);
+  });
+  it("should not throw error if path found", async () => {
+    const beforeMutateResult = await Promise.resolve()
+      .then(() => {
+        mutators.removeOne<{ name: string }, "name">("name").beforeMutate({
+          paths: [{ path: "name", schema: z.string() }],
+        });
+      })
+      .catch((e) => e);
+
+    expect(beforeMutateResult).not.toBeInstanceOf(Error);
+  });
+});
+
 describe("mutate.rewriteRenames", () => {
   it("should remove renames if points to the field that is renamed", () => {
     const rewriteRenames = mutators.removeOne<
