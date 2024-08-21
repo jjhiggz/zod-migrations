68d4521e17dc5e9f4a02424bd1c0af0a517a5836
Jon Higger <jonathan.higger@gmail.com>
2024-08-20 19:58:38 -0600

    got isValid tests for removeMany done

--- test/unit/remove-many.test.ts
+++ test/unit/remove-many.test.ts
@@ -25,8 +25,68 @@ describe("mutator.up", () => {
   });
 });
 
-describe.skip("mutator.isValid", () => {
-  // TODO
+describe("mutator.isValid", () => {
+  const isValid = mutators.addMany({
+    defaultValues: {
+      age: 1,
+      name: "jon",
+    },
+    schema: z.object({
+      name: z.string(),
+      age: z.number(),
+    }),
+  }).isValid;
+
+  it("should be valid if every key points to a path with the right schema", () => {
+    expect(
+      isValid({
+        input: {
+          name: "jon",
+          age: 1,
+        },
+        paths: ["name", "age"],
+        renames: [],
+      })
+    ).toBe(true);
+  });
+
+  it("should not be valid if some key points to a path with the wrong schema", () => {
+    expect(
+      isValid({
+        input: {
+          name: "jon",
+          age: "age string",
+        },
+        paths: ["name", "age"],
+        renames: [],
+      })
+    ).toBe(false);
+  });
+
+  it("should not be valid if a key is missing", () => {
+    expect(
+      isValid({
+        input: {
+          age: "age string",
+        },
+        paths: ["age"],
+        renames: [],
+      })
+    ).toBe(false);
+  });
+
+  it("should be valid with valid renames", () => {
+    expect(
+      isValid({
+        input: {
+          name: "jon",
+          newAge: 1,
+        },
+        paths: ["age"],
+        renames: [["age", "newAge"]],
+      })
+    ).toBe(true);
+  });
 });
 
 describe.skip("mutator.rewritePaths", () => {
