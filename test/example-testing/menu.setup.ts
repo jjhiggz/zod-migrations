c970e690e270277e0e290872f7cfb7ad1140faf2
Jon Higger <jonathan.higger@gmail.com>
2024-08-23 10:12:46 -0600

    just need to do restaurant then we're throwing this onto remenu motherfucker

--- test/example-testing/menu.setup.ts
+++ test/example-testing/menu.setup.ts
@@ -1,7 +1,100 @@
 import { z } from "zod";
+import { createZodMigrations } from "../../src/zod-migration";
+import { IsZodMigratorValid } from "../../src/types/types";
+import { Merge, Simplify } from "type-fest";
+import {
+  sectionGroupMigrator,
+  sectionGroupSchemaV4,
+  SectionGroupV1,
+  SectionGroupV2,
+  SectionGroupV3,
+  SectionGroupV4,
+} from "./section-group.setup";
 
 export const initialMenuSchema = z.object({
-  type: z.literal("MENU"),
   id: z.string(),
   name: z.string(),
+  type: z.literal("MENU"),
 });
+
+export const menuSchemaV2 = initialMenuSchema.merge(
+  z.object({
+    sectionGroups: z.array(sectionGroupSchemaV4),
+  })
+);
+
+export type MenuV1 = z.infer<typeof initialMenuSchema>;
+export type MenuV2 = Simplify<
+  Merge<
+    z.infer<typeof menuSchemaV2>,
+    {
+      sectionGroups: (SectionGroupV1 | SectionGroupV2 | SectionGroupV3)[];
+    }
+  >
+>;
+
+export type MenuV3 = Simplify<
+  Merge<
+    z.infer<typeof menuSchemaV3>,
+    {
+      sectionsGroups: (
+        | SectionGroupV1
+        | SectionGroupV2
+        | SectionGroupV3
+        | SectionGroupV4
+      )[];
+    }
+  >
+>;
+
+export type MenuV4 = z.infer<typeof menuSchemaV4>;
+
+export const menuSchemaV3 = menuSchemaV2.merge(
+  z.object({
+    subheaderFontFamily: z.string(),
+    subheaderFontSize: z.number(),
+  })
+);
+
+export const menuSchemaV4 = menuSchemaV3
+  .omit({
+    subheaderFontFamily: true,
+    subheaderFontSize: true,
+  })
+  .merge(
+    z.object({
+      sectionHeaderFontFamily: z.string(),
+      sectionHeaderFontSize: z.number(),
+    })
+  );
+
+export const menuMigrator = createZodMigrations({
+  startingSchema: initialMenuSchema,
+  endingSchema: menuSchemaV4,
+})
+  .addNestedArray({
+    nestedMigrator: sectionGroupMigrator,
+    path: "sectionGroups",
+    schema: sectionGroupSchemaV4,
+  })
+  .addMany({
+    schema: z.object({
+      subheaderFontFamily: z.string(),
+      subheaderFontSize: z.number(),
+    }),
+    defaultValues: {
+      subheaderFontFamily: "Default Font",
+      subheaderFontSize: 12,
+    },
+  })
+  .renameMany({
+    subheaderFontFamily: "sectionHeaderFontFamily",
+    subheaderFontSize: "sectionHeaderFontSize",
+  } as const);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function isValid(): true {
+  //   type InferredFromSchema = z.infer<typeof sectionSchemaV4>;
+  //   type InferredFromMigrator = ZodMigratorEndShape<typeof sectionMigrator>;
+  return true as IsZodMigratorValid<typeof sectionGroupMigrator>;
+}
